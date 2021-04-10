492525bd915b03d4ae1b1fec5fb36b6d87e07caf
quangbaorp <baonq@mcrew-tech.com>
2021-04-10 09:00:22 +0700

    logic_first

---
+++ script1.js
@@ -0,0 +1,35 @@
+var observe;
+if (window.attachEvent) {
+    observe = function (element, event, handler) {
+        element.attachEvent('on'+event, handler);
+    };
+}
+else {
+    observe = function (element, event, handler) {
+        element.addEventListener(event, handler, false);
+    };
+}
+function init () {
+    var text = document.querySelector('textarea');
+    function resize () {
+        text.style.height = 'auto';
+        var scrolltext = text.scrollHeight;
+        text.style.height = scrolltext+'px';
+        window.scrollTo(0 , scrolltext)
+    }
+    /* 0-timeout to get the already changed text */
+    function delayedResize () {
+        window.setTimeout(resize, 0);
+    }
+    observe(text, 'change',  resize);
+    observe(text, 'keypress',  resize);
+
+    observe(text, 'cut',     delayedResize);
+    observe(text, 'paste',   delayedResize);
+    observe(text, 'drop',    delayedResize);
+    observe(text, 'keydown', delayedResize);
+
+    text.focus();
+    text.select();
+    resize();
+}
